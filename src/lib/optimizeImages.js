259ab8ed56ac23b6cef2898ae8ac594169508179
Lars Graubner <mail@larsgraubner.de>
2018-04-21 22:56:46 +0200

    only process images if folder exists

--- src/lib/optimizeImages.js
+++ src/lib/optimizeImages.js
@@ -4,21 +4,28 @@ const imageminOptipng = require('imagemin-optipng')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminSvgo = require('imagemin-svgo')
 const spinner = require('ora')
+const fs = require('fs-extra')
+const { resolve } = require('path')
 
 const error = require('../utils/output/error')
 
 const optimizeImages = async (ctx: Object) => {
   const { paths } = ctx
+
+  const srcPath = resolve(paths.assets, 'images')
+  const destPath = resolve(paths.public, 'images')
+
+  const exists = await fs.pathExists(srcPath)
+  if (!exists) {
+    return
+  }
+
   const output = spinner('optimizing images').start()
 
   try {
-    await imagemin(
-      [`${paths.assets}/images/**/*.{jpg,png,svg}`],
-      `${paths.public}/images`,
-      {
-        plugins: [imageminOptipng(), imageminMozjpeg(), imageminSvgo()]
-      }
-    )
+    await imagemin([`${srcPath}/**/*.{jpg,png,svg}`], destPath, {
+      plugins: [imageminOptipng(), imageminMozjpeg(), imageminSvgo()]
+    })
     output.succeed()
   } catch (err) {
     output.fail()
